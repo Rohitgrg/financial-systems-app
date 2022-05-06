a4677ba3f551753958fc7c6a92d0969b26e20d44
Rohit Gurung <rg@Rohits-MacBook-Air.local>
2022-05-06 17:16:42 +1000

    ADD: setup user service

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './models/user.model';
+
+@Injectable()
+export class UsersService {
+    createUser(){};
+    listAllUsers(){};
+    findOneUser(){};
+    updateUser(){};
+    deleteUser(){};
+}
